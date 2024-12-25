9d08a4058d11e9f0d2bd3c414c1013b8f434baee
Hampus Vretinger <brozgamers@hotmail.com>
2024-12-25 17:53:22 +0000

    Add Login/logout views, IsOwner permissions

--- freelancers/serializers.py
+++ freelancers/serializers.py
@@ -4,6 +4,11 @@ from .models import Freelancer
 
 class FreelanceSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Freelancer
--- freelancers/views.py
+++ freelancers/views.py
@@ -4,31 +4,41 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Freelancer
 from .serializers import FreelanceSerializer
+from swifthive_api.permissions import IsOwnerOrReadOnly
 
 
 class FreelanceList(APIView):
     def get(self, request):
         freelancers = Freelancer.objects.all() 
-        serializers = FreelanceSerializer(freelancers, many=True)
+        serializers = FreelanceSerializer(
+            freelancers, many=True, context={'request': request}
+        )
         return Response(serializers.data)
 
 
 class FreelancerDetail(APIView):
     serializer_class = FreelanceSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
     def get_object(self, pk):
         try:
+            self.check_object_permissions(self.request, Freelancer)
             return Freelancer.objects.get(pk=pk)
         except Freelancer.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         freelancer = self.get_object(pk)
-        serializer = FreelanceSerializer(freelancer)
+        serializer = FreelanceSerializer(
+            freelancer, context={'request': request}
+        )
         return Response(serializer.data)
 
     def put(self, request, pk):
         freelancer = self.get_object(pk)
-        serializer = FreelanceSerializer(freelancer, data=request.data)
+        serializer = FreelanceSerializer(
+            freelancer, data=request.data, context={'request': request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
