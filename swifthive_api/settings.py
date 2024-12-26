9d9b40edaabf8c126d1a9bfbd8f9807f5ca2c5d0
Hampus Vretinger <brozgamers@hotmail.com>
2024-12-26 13:28:02 +0000

    Fix spelling in settings for JWT auth cookie

--- swifthive_api/settings.py
+++ swifthive_api/settings.py
@@ -52,7 +52,7 @@ else:
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
-JWT_AUTH_REFRESH_COOKE = 'my-refresh-token'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
