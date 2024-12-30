7da88a92cfab910bd515694f8c703180bd5993ee
Hampus Vretinger <brozgamers@hotmail.com>
2024-12-30 15:32:22 +0000

    Fix customuser error

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 from clients.models import Company  # Import the Company model from the client app
+from .models import CustomUser
 
 class CustomUserSignupForm(UserCreationForm):
     ROLE_CHOICES = [
@@ -16,5 +17,5 @@ class CustomUserSignupForm(UserCreationForm):
     )
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'email', 'password1', 'password2', 'role', 'company']
