48f2a9f8abc1ca62d577ee3a39aaa9a1bc1d50f9
Hampus Vretinger <brozgamers@hotmail.com>
2024-12-30 14:24:07 +0000

    Fix allauth middleware issue

--- swifthive_api/settings.py
+++ swifthive_api/settings.py
@@ -113,7 +113,6 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 MIDDLEWARE = [
-    'allauth.account.middleware.AccountMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
