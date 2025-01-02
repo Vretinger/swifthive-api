327508dcb049d5cc7ad4df315b79683a8353944b
Hampus Vretinger <brozgamers@hotmail.com>
2025-01-02 00:00:54 +0000

    Add CSRF Trusted origins

--- swifthive_api/settings.py
+++ swifthive_api/settings.py
@@ -129,10 +129,16 @@ if 'CLIENT_ORIGIN' in os.environ:
      CORS_ALLOWED_ORIGINS = [
          os.environ.get('CLIENT_ORIGIN')
      ]
+     CSRF_TRUSTED_ORIGINS = [
+         os.environ.get('CLIENT_ORIGIN')
+    ]
      
 if 'CLIENT_ORIGIN_DEV' in os.environ:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-         r"^https:\/\/.*\.codeinstitute-ide\.net$",
+    CORS_ALLOWED_ORIGINS = [
+         os.environ.get('CLIENT_ORIGIN_DEV')
+     ]
+    CSRF_TRUSTED_ORIGINS = [
+         os.environ.get('CLIENT_ORIGIN_DEV')
     ]
 
 CORS_ALLOW_CREDENTIALS = True
