fff14533e61f720a176a919de5e702d282c8621f
vretinger <brozgamers@hotmail.com>
2025-02-02 14:24:56 +0100

    Fix DB_URL decoding issue

--- swifthive_api/settings.py
+++ swifthive_api/settings.py
@@ -84,7 +84,7 @@ ACCOUNT_USERNAME_REQUIRED = False
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG')  == 'True'
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
