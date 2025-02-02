98cdc4d6144b71ce46b705d5dfb6ab57a5e8b832
vretinger <brozgamers@hotmail.com>
2025-02-02 14:12:37 +0100

    Set debug true

--- swifthive_api/settings.py
+++ swifthive_api/settings.py
@@ -84,7 +84,7 @@ ACCOUNT_USERNAME_REQUIRED = False
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
