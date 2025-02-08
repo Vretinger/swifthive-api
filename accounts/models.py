1f1e6b891865ffd6add3eaad818d412011e7b1ef
vretinger <brozgamers@hotmail.com>
2025-02-08 22:47:45 +0100

    Add migrations

--- accounts/models.py
+++ accounts/models.py
@@ -97,7 +97,7 @@ class Company(models.Model):
     
 
 class Listing(models.Model):
-    Company = models.ForeignKey(Company, on_delete=models.CASCADE, default="null")
+    Company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
     location = models.CharField(max_length=255)
