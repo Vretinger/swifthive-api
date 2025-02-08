04f062d1cb42509eae51c8715539f8d22cdaa7b4
vretinger <brozgamers@hotmail.com>
2025-02-08 22:23:39 +0100

    Add url for job listings

--- accounts/models.py
+++ accounts/models.py
@@ -97,7 +97,7 @@ class Company(models.Model):
     
 
 class Listing(models.Model):
-    ClientProfile = models.ForeignKey(ClientProfile, on_delete=models.CASCADE)
+    Company = models.ForeignKey(Company, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField()
     location = models.CharField(max_length=255)
--- accounts/urls.py
+++ accounts/urls.py
@@ -3,14 +3,9 @@ from .views import CustomRegisterView, FreelancerListView, FreelancerDetailView,
 from dj_rest_auth import views as dj_rest_auth_views
 
 urlpatterns = [
-    # Custom registration view
-    path('dj-rest-auth/registration/', CustomRegisterView.as_view(), name='custom-register'),
-    
-    # Default dj-rest-auth URLs (including login, logout, password reset, etc.)
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-
     path('freelancers/', FreelancerListView.as_view(), name='freelancer-list'),
     path('freelancers/<int:pk>/', FreelancerDetailView.as_view(), name='freelancer-detail'),
     path('clients/', ClientListView.as_view(), name='client-list'),
     path('clients/<int:pk>/', ClientDetailView.as_view(), name='client-detail'),
+    path('Listing/', ClientDetailView.as_view(), name='Listing-detail'),
 ]
