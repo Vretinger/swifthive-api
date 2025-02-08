25ea93c2b4a6a4897409ed482dbc3d81021da697
vretinger <brozgamers@hotmail.com>
2025-02-08 14:38:54 +0100

    Add debug for role assigning

--- accounts/signals.py
+++ accounts/signals.py
@@ -2,8 +2,11 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import CustomUser, FreelancerProfile, ClientProfile
 
+
 @receiver(post_save, sender=CustomUser)
 def create_user_profile(sender, instance, created, **kwargs):
+    print(f"Signal triggered for user: {instance.email}, created: {created}, role: {instance.role}")
+    
     if created:
         if instance.role == 'freelancer':
             FreelancerProfile.objects.create(user=instance)
