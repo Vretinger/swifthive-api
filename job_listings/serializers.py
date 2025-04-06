e74483b259b812f26a4cc82646a6a7a29cc75bdd
vretinger <brozgamers@hotmail.com>
2025-04-06 11:09:34 +0200

    Add a Created by in joblisting serializer for when creating jobb listings

--- job_listings/serializers.py
+++ job_listings/serializers.py
@@ -7,3 +7,8 @@ class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
         fields = '__all__'
+        read_only_fields = ['created_by']
+
+    def create(self, validated_data):
+        validated_data['created_by'] = self.context['request'].user
+        return super().create(validated_data)
