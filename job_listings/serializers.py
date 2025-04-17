ed65c56422064d3ad70435986b829647831397b1
vretinger <brozgamers@hotmail.com>
2025-04-17 16:34:12 +0200

    Add Application count in listing serilizer to be able to visualize how many have applied

--- job_listings/serializers.py
+++ job_listings/serializers.py
@@ -1,13 +1,17 @@
-from django.contrib.humanize.templatetags.humanize import naturaltime
+# serializers.py
 from rest_framework import serializers
 from .models import Listing
 
 class ListingSerializer(serializers.ModelSerializer):
+    applicant_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Listing
         fields = '__all__'
         read_only_fields = ['created_by']
+
+    def get_applicant_count(self, obj):
+        return obj.applications.count()
 
     def create(self, validated_data):
         validated_data['created_by'] = self.context['request'].user
