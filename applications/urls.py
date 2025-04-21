6cffc87a27283eb324f3bf3749c7f9d80d1fa31b
vretinger <brozgamers@hotmail.com>
2025-04-22 00:18:24 +0200

    Fix update application path to use PK instead of ID

--- applications/urls.py
+++ applications/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("applications/apply/", ApplyForJobAPI.as_view(), name="apply-for-job"),
     path("applications/my/", ListUserApplicationsAPI.as_view(), name="my-applications"),
     path("applications/<int:listing_id>/", ListJobApplicationsAPI.as_view(), name="job-applications"),
-    path("applications/<int:id>/", UpdateApplicationStatusAPI.as_view(), name="update-application"),
+    path("applications/<int:pk>/", UpdateApplicationStatusAPI.as_view(), name="update-application"),
 ]
