3db968b8699b6dff78668e3832c25cfab41794e7
vretinger <brozgamers@hotmail.com>
2025-05-01 16:33:34 +0200

    Add my jobb listing so client user can see all of thier created job listings

--- job_listings/urls.py
+++ job_listings/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ListListingsAPI, ViewListingAPI, CreateListingAPI, EditDeleteListingAPI
+from .views import ListListingsAPI, ViewListingAPI, CreateListingAPI, EditDeleteListingAPI, MyListingsAPI
 
 urlpatterns = [
     path('listings/', ListListingsAPI.as_view(), name='list-listings'),
     path('listings/<int:pk>/', ViewListingAPI.as_view(), name='view-listing'),  # View job listing
+    path('my-listings/', MyListingsAPI.as_view(), name="my-job-listings"),
     path('listings/<int:pk>/edit/', EditDeleteListingAPI.as_view(), name='edit-listing'),
     path('listings/<int:pk>/delete/', EditDeleteListingAPI.as_view(), name='delete-listing'),
     path('create/', CreateListingAPI.as_view(), name='create-listing'),
