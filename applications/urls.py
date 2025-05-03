d501b2f3ad9fabbaff0d190ef4f5e9718eaae033
vretinger <brozgamers@hotmail.com>
2025-05-03 12:17:25 +0200

    Add specifikation to applications url to seperet viewing list and updating applicant

--- applications/urls.py
+++ applications/urls.py
@@ -9,6 +9,6 @@ from .views import (
 urlpatterns = [
     path("apply/", ApplyForJobAPI.as_view(), name="apply-for-job"),
     path("my/", ListUserApplicationsAPI.as_view(), name="my-applications"),
-    path("<int:listing_id>/", ListJobApplicationsAPI.as_view(), name="job-applications"),
-    path("<int:pk>/", UpdateApplicationStatusAPI.as_view(), name="update-application"),
+    path("list/<int:listing_id>/", ListJobApplicationsAPI.as_view(), name="job-applications"),
+    path("update/<int:pk>/", UpdateApplicationStatusAPI.as_view(), name="update-application"),
 ]
